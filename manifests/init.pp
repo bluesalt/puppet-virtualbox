52897568cb63abfd737f1c094938b812ac5b00dd
Jeff Li <jafelee@gmail.com>
2013-03-13 13:29:27 +0800

    use virtualbox 4.2.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class virtualbox {
   package { 'VirtualBox':
     provider => 'pkgdmg',
-    source   => 'http://127.0.0.1:12345/VirtualBox-4.2.8-83876-OSX.dmg'
+    source   => 'http://127.0.0.1:12345/VirtualBox-4.2.0-80737-OSX.dmg'
   }
 }
 
